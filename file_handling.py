0434e4fff84686991b7a0b34d192ba0dcd21b3bc
vishnugundala <vishnugundala7330@gmail.com>
2023-07-18 23:11:16 +0530

    my initial changes for data_separetion

---
+++ file_handling.py
@@ -0,0 +1,9 @@
+def data_separation(path):
+    fh = open(path, "r")
+    return fh.read()
+
+
+if __name__ == "__main__":
+    path = "C:\\Users\\vishn\\OneDrive\\Desktop\\New Text Document.txt"
+    result = data_separation(path)
+    print(result)
